a77cfb76441d0e85f7948d8d63fe0d147e8b05df
dark <mail@mathiasjost.com>
2012-09-13 01:32:04 +0200

    initial commit
    initial commit of the addon files.

---
+++ eBackgroundFrames.lua
@@ -0,0 +1,163 @@
+-- =============================================================================
+--
+--       Filename:  eBackgroundFrames.lua
+--
+--    Description:  Creates frames as a ui background.
+--
+--        Version:  5.0.1
+--
+--         Author:  Mathias Jost (mail@mathiasjost.com)
+--
+-- =============================================================================
+
+
+
+
+-- -----------------------------------------------------------------------------
+-- Function for hiding the skada frames when not in a raid
+-- -----------------------------------------------------------------------------
+local function eBackgroundFrames_OnEvent(event, arg1)
+
+	if GetNumSubgroupMembers() < 1 and GetNumGroupMembers() < 1 then
+		ebfSkadaDamage:Hide()
+		ebfSkadaThreat:Hide()
+	else
+		ebfSkadaDamage:Show()
+		ebfSkadaThreat:Show()
+	end
+
+end
+
+
+-- -----------------------------------------------------------------------------
+-- Function for creating the bg frames
+-- -----------------------------------------------------------------------------
+local function FrameCreator(name, width, height, alpha, level)
+
+  -- create the local frame
+  local frame = CreateFrame("Frame", name, UIParent)
+
+  frame:SetFrameLevel(level)
+  frame:SetWidth(width)
+  frame:SetHeight(height)
+  frame:SetAlpha(alpha)
+
+  frame:SetBackdrop( {
+    bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+    edgeFile = "Interface\\AddOns\\eBackgroundFrames\\HalBorder.tga",
+    tile = false, tileSize = 0, edgeSize = 8,
+    insets = { left = 2, right = 2, top = 2, bottom = 2 }
+  })
+
+  return frame
+
+end
+
+
+-- -----------------------------------------------------------------------------
+-- Create the top Bar
+-- -----------------------------------------------------------------------------
+
+local ebfTopBar = FrameCreator("ebfTopBar", 2000, 30, 1.0, 0)
+ebfTopBar:SetFrameStrata("BACKGROUND")
+ebfTopBar:SetPoint("TOP", 0, 5)
+ebfTopBar:Show()
+
+ebfTopBar:SetScript("OnEvent", eBackgroundFrames_OnEvent)
+ebfTopBar:RegisterEvent("PLAYER_ENTERING_WORLD")
+ebfTopBar:RegisterEvent("GROUP_ROSTER_UPDATE")
+ebfTopBar:RegisterEvent("PET_BAR_UPDATE")
+
+
+-- -----------------------------------------------------------------------------
+-- Create the Minimap Frame
+-- -----------------------------------------------------------------------------
+local ebfMinimap = FrameCreator("ebfMinimap", 150, 150, 1.0, 2)
+ebfMinimap:SetPoint("TOPLEFT", 16, -6)
+ebfMinimap:Show()
+
+
+-- -----------------------------------------------------------------------------
+-- Create the Minimap Panel 1 Frame
+-- -----------------------------------------------------------------------------
+local ebfMinimapP1 = FrameCreator("ebfMinimapP1", 150, 30, 1.0, 1)
+ebfMinimapP1:SetPoint("TOPLEFT", 16, -156)
+ebfMinimapP1:Show()
+
+
+-- -----------------------------------------------------------------------------
+-- Create the Minimap Panel 2 Frame
+-- -----------------------------------------------------------------------------
+local ebfMinimapP2 = FrameCreator("ebfMinimapP2", 150, 30, 1.0, 1)
+ebfMinimapP2:SetPoint("TOPLEFT", 16, -186)
+ebfMinimapP2:Show()
+
+
+-- -----------------------------------------------------------------------------
+-- Create the Minimap Panel 3 Frame
+-- -----------------------------------------------------------------------------
+local ebfMinimapP3 = FrameCreator("ebfMinimapP3", 150, 30, 1.0, 1)
+ebfMinimapP3:SetPoint("TOPLEFT", 16, -216)
+ebfMinimapP3:Show()
+
+
+-- -----------------------------------------------------------------------------
+-- Create the Skada Threat Frame
+-- -----------------------------------------------------------------------------
+local ebfSkadaThreat = FrameCreator("ebfSkadaThreat", 150, 185, 1.0, 1)
+ebfSkadaThreat:SetPoint("TOPLEFT", 16, -246)
+ebfSkadaThreat:Show()
+
+
+-- -----------------------------------------------------------------------------
+-- Create the Skada Damage Frame
+-- -----------------------------------------------------------------------------
+local ebfSkadaDamage = FrameCreator("ebfSkadaDamage", 150, 185, 1.0, 1)
+ebfSkadaDamage:SetPoint("TOPLEFT", 16, -431)
+ebfSkadaDamage:Show()
+
+
+-- -----------------------------------------------------------------------------
+-- Create the bottom Bar
+-- -----------------------------------------------------------------------------
+local ebfBottomBar = FrameCreator("ebfBottomBar", 2000, 30, 1.0, 0)
+ebfBottomBar:SetPoint("BOTTOM", 0, -5)
+ebfBottomBar:Show()
+
+
+-- -----------------------------------------------------------------------------
+-- Create the Button Frame
+-- -----------------------------------------------------------------------------
+local ebfButtonFrame = FrameCreator("ebfButtonFrame", 455, 100, 1.0, 1)
+ebfButtonFrame:SetPoint("BOTTOM", 0, -18)
+ebfButtonFrame:Show()
+
+
+-- -----------------------------------------------------------------------------
+-- Redesign blizzard frames
+-- -----------------------------------------------------------------------------
+WorldStateAlwaysUpFrame:SetPoint("TOP", 0, -50)
+--LFGSearchStatus:ClearAllPoints()
+--LFGSearchStatus:SetPoint("TOPLEFT", MiniMapLFGFrame, "BOTTOMRIGHT", 5, 0)
+--LFGSearchStatus:SetBackdrop( {
+--    bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+--    edgeFile = "Interface\\AddOns\\eBackgroundFrames\\HalBorder.tga",
+--    tile = false, tileSize = 0, edgeSize = 8,
+--    insets = { left = 2, right = 2, top = 2, bottom = 2 }
+--  })
+
+
+WatchFrame:ClearAllPoints()
+WatchFrame:SetPoint("TOPLEFT", "UIParent", "TOPRIGHT", 350,-300)
+WatchFrame:SetHeight(600)
+WatchFrame.ClearAllPoints = function() end
+WatchFrame.SetPoint = function() end
+WatchFrame.SetAllPoints = function() end
+
+
+Boss1TargetFrame:ClearAllPoints()
+Boss1TargetFrame:SetPoint("TOPLEFT", "UIParent", "TOPRIGHT", 350,-300)
+Boss1TargetFrame.ClearAllPoints = function() end
+Boss1TargetFrame.SetPoint = function() end
+Boss1TargetFrame.SetAllPoints = function() end
+
